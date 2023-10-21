f6ad317942583dcf6e1982bbd491761b21a01cc7
cherriedy <huy5adv@gmail.com>
2023-10-22 01:40:00 +0700

    21/10/2023

--- backend/index.php
+++ backend/index.php
@@ -6,54 +6,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Dashboard</title>
+    <title>Login</title>
 
-    <!-- || GENERAL CONFIG || -->
-    <?php include_once(__DIR__ . "/../config/config.php") ?>
-    <!-- || DATABASE CONFIG || -->
-    <?php include_once(__DIR__ . '/../resources/share/database/db_connect.php') ?>
-    <?php include_once(__DIR__ . '/../resources/share/database/db_query.php') ?>
-    <!-- || GENERAL CSS || -->
+    <?php
+        // SHORTHAND: PROJECT NAME ==> maithuy
+        define('project', 'maithuy');
+        // SHORTHAND: DIRECTORY_SEPARETOR ==> DS
+        define('DS', DIRECTORY_SEPARATOR);
+        // SHORTHAND: ASSETS FOLDER PATH => DIR_ASSETS
+        define('DIR_ASSETS', $_SERVER['DOCUMENT_ROOT'] . DS . project . DS . 'assets');
+        // SHORTHAND: BACKEND FOLDER PATH => DIR_BACKEND
+        define('DIR_BACKEND', $_SERVER['DOCUMENT_ROOT'] . DS . project . DS . 'backend');
+        // SHORTHAND: FRONTEND FOLDER PATH => DIR_FRONTEND
+        define('DIR_FRONTEND', $_SERVER['DOCUMENT_ROOT'] . DS . project . DS . 'frontend');
+        // INCLUDE: CONFIG.PHP
+        include_once(DIR_ASSETS . DS . 'config' . DS . 'config.php');
+        // INCLUDE: DB_CONNECT.PHP
+        include_once($ASSETS['DIR_DATABASE'] . 'db_connect.php');
+        // INCLUDE: DB_QUERY.PHP
+        include_once($ASSETS['DIR_DATABASE'] . 'db_query.php');
+        // INCLUDE: SS_START.PHP
+        include_once($BACKEND['DIR_SESSION'] . 'ss_start.php');
+        // INCLUDE: SS_CLOSE.PHP
+        include_once($BACKEND['DIR_SESSION'] . 'ss_close.php');
+        // CALL: SESSION_BEGIN() => CHECK SESSION IS CREATED AND START SESSION
+        session_begin();
+    ?>
+
+    <!-- LINK: STYLES.CSS -->
     <link rel="stylesheet" href="./css/styles.css" type="text/css">
 </head>
 
 <body>
-    <!-- || HEADER SECTION || -->
-    <?php include_once(__DIR__ . '/../resources/admin/frontend/view/header.php') ?>
-
-    <?php 
-        if (isset($_GET['pid'])) {
-            $pid = $_GET['pid'];
-            switch($pid) {
-                case 1: // CRUD: PRODUCT - INDEX
-                    include_once(__DIR__ . '/../resources/admin/backend/functions/product/index.php');
-                    break;
-
-                case 2: // CRUD: PRODUCT GROUP - INDEX
-                    include_once(__DIR__ . '/../resources/admin/backend/functions/product_group/index.php');
-                    break;
-                
-                case 3: // CRUD: PRODUCT TYPE
-                    include_once(__DIR__ . '/../resources/admin/backend/functions/product_type/index.php');
-                    break;
-                    
-                case 4: // CRUD: PRODUCT - CREATE
-                    include_once(__DIR__ . '/../resources/admin/backend/functions/product/create.php');
-                    break;
-
-                case 5: // CRUD: PRODUCT - EDIT
-                    include_once(__DIR__ . '/../resources/admin/backend/functions/product/edit.php');
-                    break;
-
-                case 6: // CRUD: PRODUCT - DELETE
-                    include_once(__DIR__ . '/../resources/admin/backend/functions/product/delete.php');
-                    break;
-                
-                default:
-            }
-        }
-
-    ?>
+    
 </body>
 
 </html>

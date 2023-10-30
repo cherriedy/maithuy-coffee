0c9eddac0ae081f8c5c7d770756d87de6fedfa23
cherriedy <huy5adv@gmail.com>
2023-10-31 01:19:29 +0700

    31/10

---
+++ public/tests/product/prod.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="prod.css" rel="stylesheet">
+    <!-- LINK: BOXICONS CSS -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' type="text/css">
+</head>
+
+<?php 
+    include_once(realpath(dirname(__FILE__) . '/../../../resources/config/config.php'));
+    include_once(realpath(dirname(__FILE__) . '/../../../resources/database/connect.php'));
+    include_once(realpath(dirname(__FILE__) . '/../../../resources/database/query.php'));
+
+    $tb_sp = $TABLE['sp'];
+    $tb_lsp = $TABLE['lsp'];
+    $tb_nsp = $TABLE['nsp'];
+
+    $conn = db_connect();
+    $sql_select_prod_type = "SELECT TEN_LOAISP FROM $tb_lsp";
+    $sql_select_prod_group = "SELECT TEN_NHOMSP FROM $tb_nsp";
+
+    $sql_query_result_1 = db_query($conn, $sql_select_prod_group);
+?>
+
+<body>
+    <section class="product-wrapper">
+        <div class="product-header-wrapper">
+            <h1>Danh sách sản phẩm</h1>
+
+            <div class="product-type">
+                <a class="active" href="index.php?page=3">Tất cả</a>
+                <?php 
+                    $group = 1;
+                    while ($row = db_fetch_assoc($sql_query_result_1)) 
+                    {
+                ?>
+                <a href="index.php?page=3&&group=<?php echo $group++; ?>"><?php echo $row['TEN_NHOMSP']; ?></a>
+                <?php 
+                    }
+                ?>
+            </div>
+        </div>
+
+        <div class="product-content">
+                        <div class="product-card">
+                <div class="img-wrapper">
+                    <img src="./../../img/product.png" alt="prod-img">
+                </div>
+
+                <div class="product-des">
+                    <div class="product-des__tag">
+                        <span class="group">ROBUSTA</span>
+                        <span class="type">BỘT</span>
+                    </div>
+
+                    <div class="product-des__brand">
+                        <span>MAITHUY COFFEE</span>
+                    </div>
+
+                    <div class="product-des__name">
+                        <span>CÀ PHÊ CHỒN TÂY NGUYÊN</span>
+                    </div>
+
+                    <div class="product-des__origin">
+                        <i class="fa-solid fa-location-dot"></i>
+                        <span>TÂY NGUYÊN, VIỆT NAM</span>
+                    </div>
+
+                    <div class="horizontal-line"></div>
+
+                    <div class="prodcut-des__price-buy">
+                        <div class="price">
+                            <span class="discount">100.000 VND</span>
+                            <p class="original">GIÁ: <span>50.000</span> VND</p>
+                        </div>
+
+                        <div class="buy">
+                            <a href="#"><i class='bx bx-shopping-bag'></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+                        <div class="product-card">
+                <div class="img-wrapper">
+                    <img src="./../../img/product.png" alt="prod-img">
+                </div>
+
+                <div class="product-des">
+                    <div class="product-des__tag">
+                        <span class="group">ROBUSTA</span>
+                        <span class="type">BỘT</span>
+                    </div>
+
+                    <div class="product-des__brand">
+                        <span>MAITHUY COFFEE</span>
+                    </div>
+
+                    <div class="product-des__name">
+                        <span>CÀ PHÊ CHỒN TÂY NGUYÊN</span>
+                    </div>
+
+                    <div class="product-des__origin">
+                        <i class="fa-solid fa-location-dot"></i>
+                        <span>TÂY NGUYÊN, VIỆT NAM</span>
+                    </div>
+
+                    <div class="horizontal-line"></div>
+
+                    <div class="prodcut-des__price-buy">
+                        <div class="price">
+                            <span class="discount">100.000 VND</span>
+                            <p class="original">GIÁ: <span>50.000</span> VND</p>
+                        </div>
+
+                        <div class="buy">
+                            <a href="#"><i class='bx bx-shopping-bag'></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+                        <div class="product-card">
+                <div class="img-wrapper">
+                    <img src="./../../img/product.png" alt="prod-img">
+                </div>
+
+                <div class="product-des">
+                    <div class="product-des__tag">
+                        <span class="group">ROBUSTA</span>
+                        <span class="type">BỘT</span>
+                    </div>
+
+                    <div class="product-des__brand">
+                        <span>MAITHUY COFFEE</span>
+                    </div>
+
+                    <div class="product-des__name">
+                        <span>CÀ PHÊ CHỒN TÂY NGUYÊN</span>
+                    </div>
+
+                    <div class="product-des__origin">
+                        <i class="fa-solid fa-location-dot"></i>
+                        <span>TÂY NGUYÊN, VIỆT NAM</span>
+                    </div>
+
+                    <div class="horizontal-line"></div>
+
+                    <div class="prodcut-des__price-buy">
+                        <div class="price">
+                            <span class="discount">100.000 VND</span>
+                            <p class="original">GIÁ: <span>50.000</span> VND</p>
+                        </div>
+
+                        <div class="buy">
+                            <a href="#"><i class='bx bx-shopping-bag'></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+                        <div class="product-card">
+                <div class="img-wrapper">
+                    <img src="./../../img/product.png" alt="prod-img">
+                </div>
+
+                <div class="product-des">
+                    <div class="product-des__tag">
+                        <span class="group">ROBUSTA</span>
+                        <span class="type">BỘT</span>
+                    </div>
+
+                    <div class="product-des__brand">
+                        <span>MAITHUY COFFEE</span>
+                    </div>
+
+                    <div class="product-des__name">
+                        <span>CÀ PHÊ CHỒN TÂY NGUYÊN</span>
+                    </div>
+
+                    <div class="product-des__origin">
+                        <i class="fa-solid fa-location-dot"></i>
+                        <span>TÂY NGUYÊN, VIỆT NAM</span>
+                    </div>
+
+                    <div class="horizontal-line"></div>
+
+                    <div class="prodcut-des__price-buy">
+                        <div class="price">
+                            <span class="discount">100.000 VND</span>
+                            <p class="original">GIÁ: <span>50.000</span> VND</p>
+                        </div>
+
+                        <div class="buy">
+                            <a href="#"><i class='bx bx-shopping-bag'></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <!-- SCRIPT: FONTAWESOME-->
+    <script src="https://kit.fontawesome.com/7c9a6eab84.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
